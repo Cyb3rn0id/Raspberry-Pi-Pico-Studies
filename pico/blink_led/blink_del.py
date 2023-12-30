50b995c58eb621135e24fdc85b4c127d13684ce7
Giovanni 'CyB3rn0id' Bernardo <settorezero@gmail.com>
2023-12-31 00:31:37 +0100

    updated "LED"

--- pico/blink_led/blink_del.py
+++ pico/blink_led/blink_del.py
@@ -1,7 +1,13 @@
+# example from Giovanni Bernardo (@cyb3rn0id)
+# blinks the embedded led using a delay
 from machine import Pin
 from utime import sleep
 
-led = Pin(25, Pin.OUT)
+# "LED" can be used only if you've updated the micropython .uf2 to the latest versions
+# and works with both RPi Pico and RPi Pico W
+# on Raspberry Pi Pico, LED is pin 25, on Raspberry Pi Pico W LED is not connected to an
+# RP2040 physical pin since is connected to the WiFi module
+led = Pin("LED", Pin.OUT)
 
 while True:
     led.toggle()
--- pico/blink_led/blink_int.py
+++ pico/blink_led/blink_int.py
@@ -1,6 +1,12 @@
+# example from Giovanni Bernardo (@cyb3rn0id)
+# blinks the embedded led using a timer interrupt
 from machine import Pin, Timer
 
-led = Pin(25, Pin.OUT)
+# "LED" can be used only if you've updated the micropython .uf2 to the latest versions
+# and works with both RPi Pico and RPi Pico W
+# on Raspberry Pi Pico, LED is pin 25, on Raspberry Pi Pico W LED is not connected to an
+# RP2040 physical pin since is connected to the WiFi module
+led = Pin("LED", Pin.OUT)
 tim = Timer()
 def tick(timer):
     global led
